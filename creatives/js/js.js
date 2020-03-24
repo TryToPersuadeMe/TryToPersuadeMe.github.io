22f5602aca8b052ca98ca256f7d6f975dcf4ca10
Dmitry <zombak1234@yandex.ru>
2020-03-24 22:54:23 +0300

    creatives = new position of burger menu

---
+++ creatives/js/js.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+  //burger menu
+  $(".header__burger-menu").on("click", () => {
+    $(event.currentTarget).toggleClass("header__burger-menu_active");
+    $(".panel__nav").toggleClass("panel__nav_burger-state");
+  });
+
+  // slow scroll
+  $("body").on("click", '[href*="#"]', function(e) {
+    var fixed_offset = 100;
+    $("html,body")
+      .stop()
+      .animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+  });
+
+  //Image Back Ground
+  function ibg() {
+    $.each($(".ibg"), function(index, val) {
+      if ($(this).find("img").length > 0) {
+        $(this).css(
+          "background-image",
+          'url("' +
+            $(this)
+              .find("img")
+              .attr("src") +
+            '")'
+        );
+      }
+    });
+  }
+  ibg();
+});
