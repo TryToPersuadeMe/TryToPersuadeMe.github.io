6128dd8ca47c714d5a8aea69fec78dc9952f3d69
Dmitry <zombak1234@yandex.ru>
2020-06-07 02:47:17 +0300

    slider + popUP + map full width responsive

--- ABtest/js/main.js
+++ ABtest/js/main.js
@@ -1,315 +1,4 @@
 $(document).ready(() => {
-  // first screen slider
-  $(".first-screen__sliderContent").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: ".first-screen__slider",
-    draggable: false,
-    adaptiveHeight: true,
-  });
-
-  $(".first-screen__slider").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    asNavFor: ".first-screen__sliderContent",
-    dots: true,
-    speed: 500,
-    // adaptiveHeight: true,
-  });
-
-  $(".first-screen__slider").on("beforeChange", function (event, slick, currentSlide, nextSlide) {
-    if (currentSlide != nextSlide) {
-      $(".first-screen")
-        .addClass(`first-screen_background-${nextSlide}`)
-        .removeClass(`first-screen_background-${currentSlide}`);
-    }
-  });
-
-  // Appear dont when click in input form in TRAVEL ORDER block
-
-  $(".dots-list_point").on("click", function () {
-    let indexInput = $(event.currentTarget).index() / 2 - 1;
-    $(".dots-list__item").not(this).removeClass("dots-list__item_active");
-    $(".dots-list__item").eq(indexInput).addClass("dots-list__item_active");
-
-    let placeholder = {
-      color: "#0094F7",
-      fontWeight: "500",
-    };
-
-    let changePlaceholder = $(event.currentTarget).find(".placeholder");
-
-    if (indexInput == 0) {
-      changePlaceholder.text("Что вас интересует?").css(placeholder);
-    } else if (indexInput == 3) {
-      changePlaceholder.text("Чем вы хотите заняться?").css(placeholder);
-    } else if (indexInput == 4) {
-      changePlaceholder.text("Какие условия выберете?").css(placeholder);
-    } else if (indexInput == 5) {
-      changePlaceholder.text("Ваши предпочтения?").css(placeholder);
-    } else if (indexInput == 6) {
-      changePlaceholder.text("Как вас встречать?").css(placeholder);
-    }
-  });
-
-  // Category slider
-  /* purchases slider */
-$(".category").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: ".row__images-keys",
-  draggable: false,
-  swipe: false,
-  touchMove: false,
-  infinite: true,
-});
-
-$(".row__images-keys").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  asNavFor: ".category",
-  dots: false,
-  arrows: false,
-  focusOnSelect: true,
-  variableWidth: true,
-  // centerMode:false,
-
-  responsive: [
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-});
-
-// current active slide in category in small row
-// $(".category__slide_small").on("click", () => {
-//   $(".category__slide_small").not(this).removeClass("category__slide_small-active");
-//   $(event.currentTarget).addClass("category__slide_small-active");
-
-// });
-
-// add active mark when swaipe small slides in ourchases block
-$(".row__images-keys").on("afterChange", function (event, slick, currentSlide, nextSlide) {
-  $(".category__slide_small").removeClass("category__slide_small-active");
-  $(".row__images-keys").find(".slick-current").addClass("category__slide_small-active");
-});
-
-$(".row__images-keys").on("click", () => {
-  console.log($(event.target).index());
-});
-
-$(".purchases__navigation")
-  .find("li")
-  .on("click", () => {
-    if (!$(event.currentTarget).hasClass("slick-current")) {
-      $(".row__images-keys").slick("goTo", 0, true);
-      $(".category__slide_small").eq(0).addClass("slick-current");
-    }
-  });
-
-$(".purchases__slider").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: ".purchases__navigation",
-  draggable: false,
-  waitForAnimate: false,
-  swipe: false,
-  touchMove: false,
-  infinite: false,
-});
-
-$(".purchases__navigation").slick({
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  asNavFor: ".purchases__slider",
-  dots: false,
-  centerMode: false,
-  focusOnSelect: true,
-  draggable: false,
-  waitForAnimate: false,
-  swipe: false,
-  touchMove: false,
-  infinite: false,
-});
-
-// Plugin for change calendar from/to
-$('input[name="travelDate-from"],input[name="purchasesFrom"]').daterangepicker({
-  singleDatePicker: true,
-  locale: {
-    direction: "ltr",
-    format: "DD/MM/YYYY HH:mm",
-    separator: " - ",
-    applyLabel: "Apply",
-    cancelLabel: "Clear",
-    fromLabel: "From",
-    toLabel: "To",
-    customRangeLabel: "Custom",
-    daysOfWeek: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-    monthNames: [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ],
-    firstDay: 1,
-  },
-  showCustomRangeLabel: false,
-  startDate: "05/05/2020",
-  endDate: "05/11/2020",
-  opens: "right",
-  autoUpdateInput: false,
-});
-
-$('input[name="travelDate-to"],input[name="purchasesTo"]').daterangepicker({
-  singleDatePicker: true,
-  locale: {
-    direction: "ltr",
-    format: "DD/MM/YYYY HH:mm",
-    separator: " - ",
-    applyLabel: "Apply",
-    cancelLabel: "Clear",
-    fromLabel: "From",
-    toLabel: "To",
-    customRangeLabel: "Custom",
-    daysOfWeek: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-    monthNames: [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ],
-    firstDay: 1,
-  },
-  showCustomRangeLabel: false,
-  startDate: "05/05/2020",
-  endDate: "05/11/2020",
-  opens: "left",
-  autoUpdateInput: false,
-});
-
-$(
-  'input[name="travelDate-from"],input[name="travelDate-to"],input[name="purchasesTo"],input[name="purchasesFrom"]'
-).on("apply.daterangepicker", function (ev, picker) {
-  $(this).val(picker.startDate.format("DD/MM/YYYY"));
-});
-
-$(
-  'input[name="travelDate-from"],input[name="travelDate-to"],input[name="purchasesTo"],input[name="purchasesFrom"]'
-).on("cancel.daterangepicker", function (ev, picker) {
-  $(this).val("");
-});
-
-$(window).on("resize", function () {
-  $(".purchases__navigation").slick("setPosition");
-  $(".purchases__slider").slick("setPosition");
-  $(".row__images-keys").slick("setPosition");
-});
-;
-
-  // feedback slider
-
-  // Feedback slider
-
-$(".feedback__sliderColumn").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: true,
-  dots: true,
-  // fade: true,
-  dotsClass: "slider-control__dots",
-});
-
-$(".slider-control__arrow_prev").on("click", () => {
-  $(".feedback__sliderColumn").slick("slickPrev");
-});
-
-$(".slider-control__arrow_next").on("click", () => {
-  $(".feedback__sliderColumn").slick("slickNext");
-});
-
-const resizeControl = () => {
-  $(".slider-control__dots").appendTo(".slider-control");
-  if ($(window).width() < 701) {
-    $(".slider-control").appendTo(".feedback__sliderColumn");
-  } else {
-    $(".slider-control").appendTo(".feedback__photo-wrapper");
-  }
-};
-
-$(window).on("resize", function () {
-  resizeControl();
-});
-
-resizeControl();
-;
-
-
-
-  // Change  SELECT/input style/checkbox. jquery.formstyler.min.js + animate all arrows
-  $("select").styler({
-    selectPlaceholde: true,
-  });
-
-  $(".jq-selectbox__trigger-arrow").click(function () {
-    $(this).toggleClass("jq-selectbox__trigger-arrow_active");
-    $(".jq-selectbox__trigger-arrow").not(this).removeClass("jq-selectbox__trigger-arrow_active");
-  });
-
-  $('input[type="checkbox"]').styler();
-  $('input[type="checkbox"]').click(function (e) {
-    $('input[type="checkbox"]').toggleClass("jq-checkbox_active");
-    e.preventDefault();
-    $("input:checkbox").attr("disabled", true).trigger("refresh");
-  });
-
-  /* advantages Section */
-  // advantages responsive design. Hide rows with columns in low viewport width. For Appear  click on arrow
-$(".advantages__click").on("click", function () {
-    $(".advantages__row").animate({ opacity: "1" }, 500);
-    $(".advantages__click").addClass("advantages__click_hide");
-    $(".advantages__logo").addClass("advantages__logo_hide");
-});;
-
-  // mouse up
-  $(document).mouseup(() => {
-    //  close arrows in input/select
-    if ($(event.target).closest(".jq-selectbox__trigger-arrow").length == 0) {
-      $(".jq-selectbox__trigger-arrow").removeClass("jq-selectbox__trigger-arrow_active");
-    }
-  });
-  $(".map-mobile").on("click", () => {
-    $(".map-mobile").addClass("shadow");
-    $(".tour__button").addClass("tour__button_active");
-  });
-
-  // global js for navigation and webP
   
   // its check browser for suppport WEBp img
   function testWebP(callback) {
@@ -360,7 +49,7 @@ $(".advantages__click").on("click", function () {
     // $(".burger-menu").toggleClass("burger-menu_active");
     $(".burger-menu-active").animate({ opacity: 1, top: "12px" }, 300);
     $("body").toggleClass("lock");
-    $("body").toggleClass("shadow");
+    $(".overflow").toggleClass("shadow");
   });
 
   $(".navResponsive__title").click(function () {
@@ -437,7 +126,7 @@ $(".advantages__click").on("click", function () {
 
       $("body").removeClass("lock");
       $(".navResponsive__arrow").removeClass("navResponsive__arrow_active");
-      $("body").removeClass("shadow");
+      $(".overflow").removeClass("shadow");
     }
   });
 
@@ -461,5 +150,283 @@ $(".advantages__click").on("click", function () {
     $(event.currentTarget).removeClass("navigation__item_else-active");
   });
 
+
+
+// Slow scroll
+$("body").on("click", '[href*="#"]', function (e) {
+  var fixed_offset = 20;
+  $("html,body")
+    .stop()
+    .animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 500);
+  e.preventDefault();
+});;
+  // $(".slider").slick({
+//   arrow: false,
+//   dots: false,
+//      ***---responsive Height ---***
+// adaptiveHeight: false,
+//      ***--- Slides to show in slider ---***
+// slidesToShow: 1,
+//      ***--- show slides per one click on button ---***
+// slidesToScroll: 1,
+//      ***--- scroll speed ---***
+// speed: 300,
+//      ***--- type of animation ---***
+//  easing: "LINEAR",
+//      ***--- infinite scroll ---***
+// infinite: true,
+//      ***--- first slide to show ---***
+// initialSlide: 0,
+//      ***--- slides autoplay ---***
+// autoplay: false,
+// autoplaySpeed: 3000,
+// pauseOnFocus: true,
+// pauseOnHover: true,
+// pauseOnDotsHover: true,
+//      ***--- turn Off swipe in tracklist on PC ---***
+// draggable: true,
+//      ***--- turn Off swipe in tracklist on mobiles ---***
+// swipe: true,
+//      ***---  ---***
+// touchThreshold: 5,
+//      ***--- block swipe contol with swipe-focus image ---***
+// touchMove: true,
+//      ***--- block/unblock click button for swipe without expect of animate finish ---***
+// waitForAnimate: true,
+//      ***--- center mode for center slide ---***
+// centerMode: false,
+//      ***--- auto width for slides ---***
+// variableWidth: false,
+//      ***--- rows in slider track ---***
+// rows: 1,
+//      ***--- slides per row ---***
+// slidesPerRow: 1,
+//      ***---  vertical slider ---***
+// vertical: false,
+//      ***--- allows use  vertical swipe ---***
+// verticalSwiping: false,
+//      ***--- fade img instead slider ---***
+// fade: false,
+//      ***--- connect with another slider ---***
+// asNavFor: ".class"
+//      ***---  responsive breakpoints---***
+// responsive: [
+//     {
+//         breakpoint: 767,
+//         settings: {
+//             // settings here
+//         }
+//     }
+// ]
+//      ***--- arrows/dots in another div ---***
+// appendArrows:$(".class")
+// appendDots: $(".class")
+// });
+
+$(".slider__row_big").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".slider__row_small",
+  waitForAnimate: false,
+  draggable: false,
+  swipe: false,
+  touchMove: false,
+  responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        draggable: true,
+        swipe: true,
+        touchMove: true,
+      },
+    },
+  ],
+});
+
+$(".slider__row_small").slick({
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  asNavFor: ".slider__row_big",
+  arrows: false,
+  dots: false,
+  focusOnSelect: true,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+        speed: 500,
+        cssEase: "linear",
+      },
+    },
+
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        speed: 500,
+        cssEase: "linear",
+      },
+    },
+
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        speed: 500,
+        cssEase: "linear",
+      },
+    },
+  ],
+});
+
+// current active slide in category in small row
+$(".slider__slide_small").on("click", () => {
+  $(".slider__slide_small").not(this).removeClass("slider__slide_small-active");
+
+  $(event.currentTarget).addClass("slider__slide_small-active");
+});
 ;
+
+  $(".table__spoiler").on("click", () => {
+    $(event.currentTarget).children().toggleClass("arrowSpoiler_active");
+    $(".table__wrapper").slideToggle(300);
+  });
+
+  var windowHeight = $(window).height();
+
+  $(document).on("scroll", function () {
+    var self = $(".feedback__wrapper"),
+      height = self.offset().top + self.height();
+    if ($(document).scrollTop() + windowHeight >= height) {
+      $(".feedback__content").addClass("feedback__content_active");
+    }
+  });
+
+  /* popUp */
+  const popUp = $(".popUp");
+  const popUp__active = "popUp_active";
+
+  $(".popUp__button-active").on("click", () => {
+    popUp.addClass(popUp__active);
+    $("main").addClass("shadow");
+    $("body").addClass("lock");
+  });
+
+  $(".popUp__closeIcon").on("click", () => {
+    popUp.removeClass(popUp__active);
+    $("main").removeClass("shadow");
+  });
+
+  $(document).mouseup(() => {
+    // close search input
+
+    if ($(event.target).closest(popUp).length == 0) {
+      popUp.removeClass(popUp__active);
+      $("main").removeClass("shadow");
+    }
+  });
+  /* popUp end */
+
+  /* map */
+  google.maps.event.addDomListener(window, "load", init);
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    var mapOptions = {
+        zoom: 11,
+
+        center: new google.maps.LatLng(40.67, -73.94),
+
+        styles: [
+            {
+                featureType: "water",
+                elementType: "geometry",
+                stylers: [{ color: "#e9e9e9" }, { lightness: 17 }],
+            },
+            {
+                featureType: "landscape",
+                elementType: "geometry",
+                stylers: [{ color: "#f5f5f5" }, { lightness: 20 }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry.fill",
+                stylers: [{ color: "#ffffff" }, { lightness: 17 }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#ffffff" }, { lightness: 29 }, { weight: 0.2 }],
+            },
+            {
+                featureType: "road.arterial",
+                elementType: "geometry",
+                stylers: [{ color: "#ffffff" }, { lightness: 18 }],
+            },
+            {
+                featureType: "road.local",
+                elementType: "geometry",
+                stylers: [{ color: "#ffffff" }, { lightness: 16 }],
+            },
+            {
+                featureType: "poi",
+                elementType: "geometry",
+                stylers: [{ color: "#f5f5f5" }, { lightness: 21 }],
+            },
+            {
+                featureType: "poi.park",
+                elementType: "geometry",
+                stylers: [{ color: "#dedede" }, { lightness: 21 }],
+            },
+            {
+                elementType: "labels.text.stroke",
+                stylers: [{ visibility: "on" }, { color: "#ffffff" }, { lightness: 16 }],
+            },
+            {
+                elementType: "labels.text.fill",
+                stylers: [{ saturation: 36 }, { color: "#333333" }, { lightness: 40 }],
+            },
+            { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
+            {
+                featureType: "transit",
+                elementType: "geometry",
+                stylers: [{ color: "#f2f2f2" }, { lightness: 19 }],
+            },
+            {
+                featureType: "administrative",
+                elementType: "geometry.fill",
+                stylers: [{ color: "#fefefe" }, { lightness: 20 }],
+            },
+            {
+                featureType: "administrative",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#fefefe" }, { lightness: 17 }, { weight: 1.2 }],
+            },
+        ],
+    };
+
+    var mapElement = document.getElementById("map");
+
+    var map = new google.maps.Map(mapElement, mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(40.67, -73.94),
+        map: map,
+        title: "Snazzy!",
+    });
+};
 });
