d9c1aef25bf725d0c1f20ef014a9e8ca298fc420
Dmitry <zombak1234@yandex.ru>
2020-06-20 22:44:31 +0300

    123

---
+++ insidePageAB-2page/js/flipCards.js
@@ -0,0 +1,23 @@
+/* qwe */
+/* CARDS responsive START */
+const resizeFlipCards = () => {
+  if ($(window).width() < 1025) {
+    $(".flipcard").appendTo(".offers__wrapperResponsive-js");
+    /* show on button click */
+    $(function () {
+      $(".flipcard").slice(0, 4).show();
+      $(".offers__button-js").on("click", function (element) {
+        element.preventDefault();
+        $(".flipcard:hidden").slice(0, 4).slideDown();
+      });
+    });
+  } else {
+    $(".offers__row-js").append((i) => $(".flipcard").slice(i * 3, (i + 1) * 3));
+  }
+};
+resizeFlipCards();
+
+$(window).on("resize", function () {
+  resizeFlipCards();
+  console.log("resizeFlipCards-3");
+});
---
+++ insidePageAB-2page/js/formStylerCustom.js
@@ -0,0 +1,45 @@
+// Change  SELECT/input style/checkbox. jquery.formstyler.min.js + animate all arrows
+$("select").styler({
+  selectPlaceholde: true,
+});
+
+$(".jq-selectbox__trigger-arrow").click(function () {
+  $(this).toggleClass("jq-selectbox__trigger-arrow_active");
+  $(".jq-selectbox__trigger-arrow").not(this).removeClass("jq-selectbox__trigger-arrow_active");
+});
+
+// mouse up
+$(document).mouseup(() => {
+  //  close arrows in input/select
+  if ($(event.target).closest(".jq-selectbox__trigger-arrow").length == 0) {
+    $(".jq-selectbox__trigger-arrow").removeClass("jq-selectbox__trigger-arrow_active");
+  }
+});
+$('input[type="reset"]').styler();
+$('input[type="checkbox"]').styler();
+$('input[type="checkbox"]').click(function (e) {
+  $('input[type="checkbox"]').toggleClass("jq-checkbox_active");
+  e.preventDefault();
+  $("input:checkbox").attr("disabled", true).trigger("refresh");
+});
+
+$(".findFilter-form__fieldWrapper").on("click", function () {
+  let indexInput = $(event.currentTarget).index();
+  console.log(indexInput);
+
+  // $(".dots-list__item").not(this).removeClass("dots-list__item_active");
+  // $(".dots-list__item").eq(indexInput).addClass("dots-list__item_active");
+
+  let placeholder = {
+    color: "#0094F7",
+    fontWeight: "500",
+  };
+
+  let changePlaceholder = $(event.currentTarget).find(".placeholder");
+
+  if (indexInput == 0) {
+    changePlaceholder.text("Что вас интересует?").css(placeholder);
+  } else if (indexInput == 1) {
+    changePlaceholder.text("Какова конечная цель?").css(placeholder);
+  }
+});
