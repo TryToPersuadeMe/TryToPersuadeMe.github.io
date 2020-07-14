05837cfffbfbbe81f0515f6b6dd562459700b5ea
Dmitry <zombak1234@yandex.ru>
2020-07-14 18:29:10 +0300

    news

--- cardItemAB/js/tableSpoiler.js
+++
@@ -1,4 +0,0 @@
-$(".table__spoiler").on("click", () => {
-    $(event.currentTarget).children().toggleClass("arrowSpoiler_active");
-    $(".table__wrapper").slideToggle(300);
-});
--- newsAB/js/main.js
+++ newsAB/js/main.js
@@ -201,4 +201,32 @@ $(document).on("scroll", function () {
 });;
 
   /* arrow back in prev page END */
+  /* article responsive end */
+
+/* CARDS responsive START */
+const resizeNews = () => {
+  if ($(window).width() > 769) {
+    $(function () {
+      $(".news").slice(0, 6).show();
+      $(".box__button").on("click", function (element) {
+        element.preventDefault();
+        $(".news:hidden").slice(0, 6).slideDown();
+      });
+    });
+  } else {
+    $(function () {
+      $(".news").slice(0, 4).show();
+      $(".box__button").on("click", function (element) {
+        element.preventDefault();
+        $(".news:hidden").slice(0, 4).slideDown();
+      });
+    });
+  }
+};
+resizeNews();
+
+$(window).on("resize", function () {
+  resizeNews();
+});
+;
 });
