7b9baeb675ae73d608f851d8bf9e808f5e2083c6
Dmitry <zombak1234@yandex.ru>
2020-07-20 17:47:34 +0300

    new

--- news_searchAmericanButler/js/main.js
+++ news_searchAmericanButler/js/main.js
@@ -161,8 +161,6 @@ $("body").on("click", '[href*="#"]', function (e) {
   e.preventDefault();
 });;
 
-
-
   /* arrow back in prev page START */
 
   var windowHeight = $(window).height();
@@ -217,5 +215,34 @@ $(".findFilter__select-wrapper").on("click", function () {
     changePlaceholder.text("Категория").css(placeholder);
   }
 });
+;
 
+  /* article responsive end */
+
+/* CARDS responsive START */
+const resizeNews = () => {
+  if ($(window).width() > 769) {
+    $(function () {
+      $(".news").slice(0, 3).show();
+      $(".box__button").on("click", function (element) {
+        element.preventDefault();
+        $(".news:hidden").slice(0, 3).slideDown();
+      });
+    });
+  } else {
+    $(function () {
+      $(".news").slice(0, 4).show();
+      $(".box__button").on("click", function (element) {
+        element.preventDefault();
+        $(".news:hidden").slice(0, 3).slideDown();
+      });
+    });
+  }
+};
+resizeNews();
+
+$(window).on("resize", function () {
+  resizeNews();
+});
+;
 });
