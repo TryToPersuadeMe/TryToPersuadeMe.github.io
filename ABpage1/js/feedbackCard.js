c2b54a74628e1697fb6a0db2bd4009afc206fafd
Dmitry <zombak1234@yandex.ru>
2020-07-29 17:09:17 +0300

    йцу

--- ABpage1/js/feedbackCard.js
+++
@@ -1,9 +0,0 @@
-var windowHeight = $(window).height();
-
-$(document).on("scroll", function () {
-    var self = $(".feedback__wrapper"),
-        height = self.offset().top + self.height();
-    if ($(document).scrollTop() + windowHeight >= height) {
-        $(".feedback__content").addClass("feedback__content_active");
-    }
-});
