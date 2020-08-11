063272fee8f7866f4e524ddffe8e818173a4090c
Dmitry <zombak1234@yandex.ru>
2020-08-11 20:08:44 +0300

    animation for sort

--- feedbackPage/js/main.js
+++ feedbackPage/js/main.js
@@ -544,71 +544,106 @@ const sortHeader = {
   str: (a, b) => a.text().localeCompare(b.text()),
 };
 
+/* animation */
+
+const feedbackAnimation = (state) => {
+  state === 0
+    ? $(".feedbackItem").animate({ opacity: 0 }, 200)
+    : $(".feedbackItem").animate({ opacity: 1 }, 200);
+};
+
 $(".sort-Header-js").click(function () {
   sortIcon();
+  feedbackAnimation(0);
+  setTimeout(() => {
+    const $this = $(this),
+      data = $this.data(),
+      compare = sortHeader[data.type],
+      field = `.${data.field}`,
+      order = +data.order || 1;
 
-  const $this = $(this),
-    data = $this.data(),
-    compare = sortHeader[data.type],
-    field = `.${data.field}`,
-    order = +data.order || 1;
+    const sortedArr = $(".feedbackItem").sort((a, b) => order * compare($(field, a), $(field, b)));
 
-  const sortedArr = $(".feedbackItem").sort((a, b) => order * compare($(field, a), $(field, b)));
+    checkSlickWidth__appendTo(sortedArr);
 
-  checkSlickWidth__appendTo(sortedArr);
+    $this.data("order", order * -1);
+  }, 300);
 
-  $this.data("order", order * -1);
+  setTimeout(() => {
+    feedbackAnimation(1);
+  }, 400);
 });
 
 /* sort by rating */
 $(".sort-Rating-js").click(function () {
   sortIcon();
+  feedbackAnimation(0);
 
-  // получаем порядок сортировки
-  var order = parseInt($(this).data("order"), 10);
+  setTimeout(() => {
+    // получаем порядок сортировки
+    var order = parseInt($(this).data("order"), 10);
 
-  // сортируем все элементы li, которые имеют атрибут data-sort
-  const sortedArr = $(".feedbackItem").sort(function (a, b) {
-    var x = parseInt($(b).data("rating"), 10);
-    var y = parseInt($(a).data("rating"), 10);
-    return (x < y ? -1 : 1) * order;
-  });
+    // сортируем все элементы li, которые имеют атрибут data-sort
+    const sortedArr = $(".feedbackItem").sort(function (a, b) {
+      var x = parseInt($(b).data("rating"), 10);
+      var y = parseInt($(a).data("rating"), 10);
+      return (x < y ? -1 : 1) * order;
+    });
 
-  checkSlickWidth__appendTo(sortedArr);
-  // инвертируем порядок сортировки
-  $(this).data("order", order * -1);
+    checkSlickWidth__appendTo(sortedArr);
+    // инвертируем порядок сортировки
+    $(this).data("order", order * -1);
+  }, 300);
+
+  setTimeout(() => {
+    feedbackAnimation(1);
+  }, 400);
 });
 
 /* sort by Date */
 
 $(".sort-Date-js").click(function () {
   sortIcon();
+  feedbackAnimation(0);
 
-  // получаем порядок сортировки
-  var order = parseInt($(this).data("order"), 10);
+  setTimeout(() => {
+    // получаем порядок сортировки
+    var order = parseInt($(this).data("order"), 10);
 
-  // сортируем все элементы li, которые имеют атрибут data-sort
-  const sortedArr_years = $(".feedbackItem").sort(function (a, b) {
-    var x = parseInt($(b).data("year"), 10);
-    var y = parseInt($(a).data("year"), 10);
-    return (x < y ? -1 : 1) * order;
-  });
+    // сортируем все элементы li, которые имеют атрибут data-sort
+    const sortedArr_years = $(".feedbackItem").sort(function (a, b) {
+      var x = parseInt($(b).data("year"), 10);
+      var y = parseInt($(a).data("year"), 10);
+      return (x < y ? -1 : 1) * order;
+    });
 
-  const sortedArr_month = sortedArr_years.sort(function (a, b) {
-    var x = parseInt($(b).data("month"), 10);
-    var y = parseInt($(a).data("month"), 10);
-    return (x < y ? -1 : 1) * order;
-  });
+    const sortedArr_month = sortedArr_years.sort(function (a, b) {
+      var x = parseInt($(b).data("month"), 10);
+      var y = parseInt($(a).data("month"), 10);
+      return (x < y ? -1 : 1) * order;
+    });
 
-  const sortedArr_day = sortedArr_month.sort(function (a, b) {
-    var x = parseInt($(b).data("month"), 10);
-    var y = parseInt($(a).data("month"), 10);
-    return (x < y ? -1 : 1) * order;
-  });
+    const sortedArr_day = sortedArr_month.sort(function (a, b) {
+      var x = parseInt($(b).data("month"), 10);
+      var y = parseInt($(a).data("month"), 10);
+      return (x < y ? -1 : 1) * order;
+    });
 
-  checkSlickWidth__appendTo(sortedArr_day);
-  // инвертируем порядок сортировки
-  $(this).data("order", order * -1);
+    checkSlickWidth__appendTo(sortedArr_day);
+    // инвертируем порядок сортировки
+    $(this).data("order", order * -1);
+  }, 300);
+
+  setTimeout(() => {
+    feedbackAnimation(1);
+  }, 400);
 });
 ;
+
+
+const qwe = () =>{
+  $(".feedbackItem__userPhoto").next().addClass("feedbackItem__userInfo-wrapper")
+
+}
+  qwe()
 });
