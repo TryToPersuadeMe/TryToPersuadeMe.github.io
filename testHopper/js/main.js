9b048f35e86917a883ba4a23fdcde842b6fe4dc2
Dmitry <zombak1234@yandex.ru>
2020-08-21 15:53:19 +0300

    asd

--- testHopper/js/main.js
+++ testHopper/js/main.js
@@ -106,64 +106,40 @@ function validate() {
 /* alert with local storage */
 let allInputsArray = document.querySelectorAll("." + classes.input);
 allInputsArray = Array.from(allInputsArray).map((item) => item.getAttribute("id"));
-const form = document.querySelector(".form");
+
 const alertBanner = document.querySelector(".alert");
 
 document.addEventListener("DOMContentLoaded", () => {
   for (let i of allInputsArray) {
     let input = document.getElementById(i);
     input.value = localStorage.getItem(i);
-
-    /* get form status */
-    form.setAttribute("data-status", localStorage.getItem("formStatus"));
     (function (i, input) {
-      input.addEventListener("input", function () {
+      input.addEventListener("change", function () {
         localStorage.setItem(i, input.value);
-        /* change form status  */
-        form.setAttribute("data-status", "changed");
-        localStorage.setItem("formStatus", form.dataset.status);
-        console.log(localStorage);
       });
     })(i, input);
   }
 
-  /* check after save any value and reload page */
-  /* data-status will be always in local storage */
-  if (localStorage.length > 1) {
+  if (localStorage.length > 0) {
+    slideToggle("alert", alertBanner, false);
     for (let index = 0; index < inputsValidate.length; index++) {
       validate.call(inputsValidate[index]);
     }
   }
-
-
 });
 
-let submitState = form.dataset.status;
-// console.log(submitState);
-
-const slideToggle = (wrapper, show) => {
-  show ? (wrapper.style.maxHeight = 0) : (wrapper.style.maxHeight = wrapper.scrollHeight + "px");
+const slideToggle = (selector, wrapper, state) => {
+  if (state) {
+    wrapper.classList.remove(selector + "_active");
+    wrapper.style.maxHeight = 0;
+  } else {
+    wrapper.classList.add(selector + "_active");
+    wrapper.style.maxHeight = wrapper.scrollHeight + "px";
+  }
 };
 
-
-if (submitState === "submited") {
-  slideToggle(alertBanner, true);
-  console.log("Asd");
-} else {
-  slideToggle(alertBanner, false);
-}
-
-
-form.addEventListener("submit", function (event) {
+const form = document.querySelector(".form").addEventListener("submit", function (event) {
   invalidInputsWrapper = document.querySelectorAll("." + classes.fieldWrapper + "_invalid");
-  if (invalidInputsWrapper.length) {
-    event.preventDefault();
-  } else {
-    form.setAttribute("data-status", "submited");
-    slideToggle(alertBanner, true);
-  
-  }
+  invalidInputsWrapper.length ? event.preventDefault() : slideToggle("alert", alertBanner, true);
 });
-
-// localStorage.clear()
 ;
