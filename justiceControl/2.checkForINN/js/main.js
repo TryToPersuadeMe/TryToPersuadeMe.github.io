c46385c60c218b61ab6675976d8839c363ead176
Dmitry <zombak1234@yandex.ru>
2020-11-22 01:53:42 +0300

    w

--- justiceControl/2.checkForINN/js/main.js
+++ justiceControl/2.checkForINN/js/main.js
@@ -1,53 +1,3 @@
-class Spoiler {
-  constructor(props) {
-    this.spoiler = document.querySelector(props.spoiler);
-    this.arrow = document.querySelector(props.arrow);
-    this.dropDown = document.querySelector(props.dropDown);
-    this.button = document.querySelector(props.button);
-    this.q = document.querySelectorAll(props.dropDown);
-
-    this.handleClick();
-    this.hanldeClickWindow();
-  }
-
-  openState() {
-    this.spoiler.classList.add("spoiler_open");
-  }
-
-  closeState() {
-    this.spoiler.classList.remove("spoiler_open");
-  }
-
-  toggleState() {
-    if (this.spoiler.classList.contains("spoiler_open")) {
-      this.closeState();
-    } else {
-      this.openState();
-    }
-  }
-
-  handleClick() {
-    this.button.addEventListener("click", () => {
-      this.toggleState();
-    });
-  }
-
-  hanldeClickWindow() {
-    window.addEventListener("mouseup", () => {
-      if (event.target != this.button) {
-        this.closeState();
-      }
-    });
-  }
-}
-
-let spoiler = new Spoiler({
-  spoiler: ".spoiler",
-  button: ".spoiler__button",
-  arrow: ".spoiler__arrow",
-  dropDown: ".spoiler__list",
-});
-;
 class SpoilerTab {
   constructor(props) {
     this.spoiler = document.querySelectorAll(props.spoiler);
@@ -280,4 +230,107 @@ let range1 = new rSlider({
   tick: 1,
   // stateText: ".state__text",
 });
+;
+class AuthorizationForm {
+  constructor(props) {
+    this.authorizationForm = document.querySelector(props.authorizationForm);
+    this.tab = document.querySelectorAll(props.tabs);
+    this.tab_inactive = props.tab_inactive;
+    this.loginForm = document.querySelector(props.loginForm);
+    this.registrationForm = document.querySelector(props.registrationForm);
+    this.tabClick();
+
+    /* popUp */
+    this.loginButton = document.querySelector(".login-js__loginButton");
+    this.registrationButton = document.querySelector(".login-js__registration");
+    this.body = document.getElementsByTagName("body")[0];
+    this.wrapper = document.querySelector(props.wrapper);
+    this.popUp();
+
+    /* close */
+    this.closeIcon = document.querySelector(props.closeIcon);
+    this.windowClick();
+    this.clickCloseIcon();
+  }
+  tabClick() {
+    for (let index = 0; index < this.tab.length; index++) {
+      this.tab[index].addEventListener("click", () => {
+        if (this.tab[index].classList.contains("authorizationForm__tab-login-js")) {
+          this.authorizationForm.classList.add("authorizationForm_login");
+          this.tab[0].classList.add(this.tab_inactive);
+          this.tab[1].classList.remove(this.tab_inactive);
+        } else {
+          this.authorizationForm.classList.remove("authorizationForm_login");
+          this.tab[1].classList.add(this.tab_inactive);
+          this.tab[0].classList.remove(this.tab_inactive);
+        }
+      });
+    }
+  }
+
+  popUp() {
+    this.loginButton.addEventListener("click", () => {
+      this.authorizationForm.classList.add("authorizationForm_active");
+      this.body.classList.add("body-lock");
+      this.wrapper.classList.add("wrapper_shadow");
+    });
+
+    this.registrationButton.addEventListener("click", () => {
+      this.authorizationForm.classList.add("authorizationForm_active");
+      this.body.classList.add("body-lock");
+      this.wrapper.classList.add("wrapper_shadow");
+    });
+  }
+
+  closePopUp() {
+    this.authorizationForm.classList.remove("authorizationForm_active");
+
+    this.body.classList.remove("body-lock");
+    this.wrapper.classList.remove("wrapper_shadow");
+  }
+
+  windowClick() {
+    window.addEventListener("click", () => {
+      if (event.target == this.wrapper) {
+        this.closePopUp();
+      }
+    });
+  }
+
+  clickCloseIcon() {
+    this.closeIcon.addEventListener("click", () => {
+      this.closePopUp();
+    });
+  }
+}
+
+const authorizationForm = new AuthorizationForm({
+  authorizationForm: ".authorizationForm",
+  tabs: ".authorizationForm__tab",
+  tab_inactive: "authorizationForm__tab_inactive",
+
+  loginForm: ".authorizationForm__loginFormWrapper",
+  registrationForm: ".authorizationForm__registrationFormWrapper",
+
+  closeIcon: ".authorizationForm__closeIcon",
+
+  wrapper: ".wrapper",
+});
+
+const togglePasswodVisible = () => {
+  const showIcon = document.querySelectorAll(".show-password-js");
+  for (let index = 0; index < showIcon.length; index++) {
+    showIcon[index].addEventListener("click", () => {
+      if (showIcon[index].classList.contains("show-password-js-visible")) {
+        showIcon[index].previousElementSibling.type = "password";
+        showIcon[index].classList.remove("show-password-js-visible");
+      } else {
+        showIcon[index].previousElementSibling.type = "text";
+        showIcon[index].classList.add("show-password-js-visible");
+      }
+    });
+  }
+};
+
+togglePasswodVisible();
 ;
