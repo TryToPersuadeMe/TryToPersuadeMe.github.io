a8e6ec724776acf8c03b08ce8691f97c1af1b974
Dmitry <zombak1234@yandex.ru>
2021-02-07 11:58:40 +0300

    1

--- explem/card/js/main.js
+++ explem/card/js/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
 
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
@@ -521,4 +523,38 @@ var popUp_sizes = new CallWindows({
   button_close: ".popUp__closePopup",
   parent: ".screen"
 });
+;
+
+var slowScrolltoAnchors = function slowScrolltoAnchors() {
+  var anchors = document.querySelectorAll('a[href*="#"]');
+
+  var _iterator = _createForOfIteratorHelper(anchors),
+      _step;
+
+  try {
+    var _loop = function _loop() {
+      var anchor = _step.value;
+      anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        var blockID = anchor.getAttribute("href").substr(1);
+        document.getElementById(blockID).scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        });
+      });
+    };
+
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      _loop();
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+
+  console.log("asd");
+};
+
+slowScrolltoAnchors();
 ;
