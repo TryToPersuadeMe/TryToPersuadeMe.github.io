198f2e9440473ea19f600340455e9e272b0fcb82
TryToPersuadeMe <61381766+TryToPersuadeMe@users.noreply.github.com>
2021-02-24 19:04:12 +0300

    Add files via upload

--- trainSet/mainPage/js/main.js
+++ trainSet/mainPage/js/main.js
@@ -1,3 +1,77 @@
+<<<<<<< HEAD
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: "auto",
+  speed: 1000,
+  loop: true,
+  autoplay: {
+    delay: 7000,
+  },
+});
+;
+class responsiveHeader {
+  constructor(props) {
+    this.navigation = document.querySelector("." + props.navigation);
+    this.burgerIcon = document.querySelector("." + props.burgerIcon);
+    this.burgerIcon_active = props.burgerIcon_active;
+
+    this.htmlBody = document.querySelector("body");
+
+    this.BurgerClick();
+    this.WindowClick();
+  }
+
+  openState() {
+<<<<<<< HEAD:trainSet/mainPage/js/main.js
+    this.navigation.classList.add("responsiveMenu_active");
+=======
+    this.navigation.classList.add("active");
+>>>>>>> f935f812fd7300c2365d9124319a7dcba95c545b:goodStone/innerContent/js/main.js
+    this.burgerIcon.classList.add(this.burgerIcon_active);
+    this.htmlBody.classList.add("bodyOverlay");
+  }
+
+  closeState() {
+<<<<<<< HEAD:trainSet/mainPage/js/main.js
+    this.navigation.classList.remove("responsiveMenu_active");
+=======
+    this.navigation.classList.remove("active");
+>>>>>>> f935f812fd7300c2365d9124319a7dcba95c545b:goodStone/innerContent/js/main.js
+    this.burgerIcon.classList.remove(this.burgerIcon_active);
+    this.htmlBody.classList.remove("bodyOverlay");
+  }
+
+  BurgerClick() {
+    this.burgerIcon.addEventListener("click", () => {
+      if (!event.currentTarget.classList.contains(this.burgerIcon_active)) {
+        this.openState();
+      } else {
+        this.closeState();
+      }
+    });
+  }
+
+  WindowClick() {
+    document.addEventListener("click", () => {
+      if (event.target.classList.contains("bodyOverlay")) {
+        this.closeState();
+      }
+    });
+  }
+}
+
+const headerBurgerMenu = new responsiveHeader({
+<<<<<<< HEAD:trainSet/mainPage/js/main.js
+  navigation: "responsiveMenu",
+  burgerIcon: "burgerMenu",
+  burgerIcon_active: "burgerMenu_active",
+=======
+  navigation: "navigation",
+  burgerIcon: "burgerIcon",
+  burgerIcon_active: "burgerIcon_active",
+>>>>>>> f935f812fd7300c2365d9124319a7dcba95c545b:goodStone/innerContent/js/main.js
+});
+;
+=======
 class responsiveHeader {
   constructor(props) {
     this.navigation = document.querySelector("." + props.navigation);
@@ -47,3 +121,4 @@ const headerBurgerMenu = new responsiveHeader({
   burgerIcon_active: "burgerMenu_active",
 });
 ;
+>>>>>>> f935f812fd7300c2365d9124319a7dcba95c545b
--- trainSet/projects/js/main.js
+++ trainSet/projects/js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   speed: 600,
@@ -18,3 +19,25 @@ var swiper = new Swiper(".swiper-container", {
   },
 });
 ;
+=======
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  speed: 600,
+  allowTouchMove: false,
+  autoHeight: true,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: false,
+    hide: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + ("Level " + index) + "</span>";
+    },
+    clickable: true,
+  },
+});
+;
+>>>>>>> f935f812fd7300c2365d9124319a7dcba95c545b
