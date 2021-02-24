a9781a03b9ed6356009a9734df41d31fd1984cf7
TryToPersuadeMe <61381766+TryToPersuadeMe@users.noreply.github.com>
2021-02-24 18:54:16 +0300

    t

--- AmericanButler/ABpage2/js/advantagesSection.js
+++
@@ -1,6 +0,0 @@
-// advantages responsive design. Hide rows with columns in low viewport width. For Appear  click on arrow
-$(".advantages__click").on("click", function () {
-    $(".advantages__row").animate({ opacity: "1" }, 500);
-    $(".advantages__click").addClass("advantages__click_hide");
-    $(".advantages__logo").addClass("advantages__logo_hide");
-});
