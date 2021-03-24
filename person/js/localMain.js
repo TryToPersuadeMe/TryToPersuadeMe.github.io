68622b7e3dc1cb2c776a57af4bd0a35b7f3b4aa9
TryToPersuadeMe <61381766+TryToPersuadeMe@users.noreply.github.com>
2021-03-24 13:00:36 +0300

    1

--- person/js/localMain.js
+++ person/js/localMain.js
@@ -32,6 +32,8 @@ var personalCardCustomScroll = new Swiper(".personalCard__containerInfo", {
   direction: "vertical",
   slidesPerView: "auto",
   freeMode: true,
+  freeModeMomentum: false,
+  freeModeMomentumBounce: false,
   scrollbar: {
     el: ".personalCard__scrollBar",
   },
