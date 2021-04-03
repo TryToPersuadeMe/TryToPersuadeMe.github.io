2f6280b4bb9a683e36913e4197d4bb77ecc20dad
Dmitry <zombak1234@yandex.ru>
2021-04-03 21:53:27 +0300

    1

--- topBro/mainPage/js/localMain.js
+++ topBro/mainPage/js/localMain.js
@@ -39,32 +39,32 @@ var commentsSlider = new Swiper(".ourClientsSection__comments-slider ", {
   autoHeight: true,
   on: {
     init: function () {
-      this.$wrapperEl[0].insertAdjacentHTML("afterbegin", `<div class="swiper-slide dummySlide comment"></div>`);
-      // this.$wrapperEl[0].insertAdjacentHTML("afterbegin", `<div class="swiper-slide dummySlide clientInfo"></div>`);
+      // this.$wrapperEl[0].insertAdjacentHTML("afterbegin", `<div class="swiper-slide dummySlide comment"></div>`);
 
       this.updateProgress();
       this.updateSize();
       this.updateSlides();
     },
-    beforeTransitionStart: function () {
-      if (this.activeIndex <= 1) {
-        this.allowSlidePrev = false;
-      } else {
-        this.allowSlidePrev = true;
-      }
-    },
+    // beforeTransitionStart: function () {
+    //   if (this.activeIndex <= 1) {
+    //     this.allowSlidePrev = false;
+    //   } else {
+    //     this.allowSlidePrev = true;
+    //   }
+    // },
 
-    activeIndexChange: function () {
-      if (this.activeIndex == 0) {
-        this.slideTo(1);
-      }
-    },
+    // activeIndexChange: function () {
+    //   if (this.activeIndex == 0) {
+    //     this.slideTo(1);
+    //   }
+    // },
   },
 });
 
 var clientsSlider = new Swiper(".ourClientsSection__clienInfo-slider", {
+  spaceBetween: 50,
   speed: 600,
-  slidesPerView: "auto",
+  // slidesPerView: "auto",
   centeredSlides: true,
   navigation: {
     nextEl: ".comment-button-next",
@@ -73,42 +73,43 @@ var clientsSlider = new Swiper(".ourClientsSection__clienInfo-slider", {
 
   breakpoints: {
     320: {
-      // slidesPerView: 1,
-      spaceBetween: 10,
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    376: {
-      spaceBetween: 30,
+
+    481: {
+      slidesPerView: "auto",
     },
-    // 481: {
-    //   slidesPerView: "auto",
-    // },
   },
 
   on: {
     init: function () {
-      this.$wrapperEl[0].insertAdjacentHTML("beforeend", `<div class="swiper-slide dummySlide clientInfo"></div>`);
+      // this.$wrapperEl[0].insertAdjacentHTML("beforeend", `<div class="swiper-slide dummySlide clientInfo"></div>`);
 
       this.updateProgress();
       this.updateSize();
       this.updateSlides();
     },
-    beforeTransitionStart: function () {
-      if (this.activeIndex <= 1) {
-        this.allowSlidePrev = false;
-      } else {
-        this.allowSlidePrev = true;
-      }
-    },
+    // beforeTransitionStart: function () {
+    //   if (this.activeIndex <= 1) {
+    //     this.allowSlidePrev = false;
+    //   } else {
+    //     this.allowSlidePrev = true;
+    //   }
+    // },
 
-    activeIndexChange: function () {
-      if (this.activeIndex == 0) {
-        this.slideTo(1);
-      }
-    },
+    // activeIndexChange: function () {
+    //   if (this.activeIndex == 0) {
+    //     this.slideTo(1);
+    //   }
+    // },
   },
 });
 
 commentsSlider.controller.control = clientsSlider;
 clientsSlider.controller.control = commentsSlider;
-clientsSlider.slideTo(Math.round(3));
+clientsSlider.slideTo(Math.round(clientsSlider.slides.length / 2));
+
+const q = document.querySelectorAll(".comment");
+const qw = document.querySelectorAll(".clientInfo");
 ;
