0b87b2248106c5650771e3411c41f797b7c4f3f0
TryToPersuadeMe <61381766+TryToPersuadeMe@users.noreply.github.com>
2021-04-15 13:41:37 +0300

    1

--- topBro/allCases/js/localMain.js
+++
@@ -1,9 +0,0 @@
-wow = new WOW({
-  boxClass: "wow", // default
-  animateClass: "animate__fadeInUpBig", // default
-  offset: 0, // default
-  mobile: true, // default
-  live: true, // default
-});
-wow.init();
-;
